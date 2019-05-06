8da264d5fefeee68fa2a27088a5c1e097ac40380
Leonardo Davinc Silva <davinc131@hotmail.com>
2019-05-06 10:36:10 -0300

    Adicionar arquivos de projeto.

---
+++ Ler_Comparar_Planilha_Excel/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ler_Comparar_Planilha_Excel
+{
+    /// <summary>
+    /// Interação lógica para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
